1572543c1f8a2f5edae8127bf05f2bf2d6d0cda3
George Lobo <77250912+wolff453@users.noreply.github.com>
2021-06-28 12:00:01 -0300

    Update instanceConnection.js

--- src/util/instanceConnection.js
+++ src/util/instanceConnection.js
@@ -2,10 +2,6 @@ class instanceConnection{
     constructor(connectionString){
         this.connectionString = connectionString
     }
-
-    async connect(){
-         return this
-    }
 }
 
 module.exports = instanceConnection
